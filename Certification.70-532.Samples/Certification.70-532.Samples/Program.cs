d1c98325a89b24112fe2b7e86eed648cee018b46
Gustavo Passos <passosgu@sempreit.com.br>
2018-02-10 23:07:21 -0200

    Adding the sample solution

---
+++ Certification.70-532.Samples/Certification.70-532.Samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification._70_532.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
